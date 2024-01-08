0efc5e31786a24e559ff04ae3bb083661763f292
Geane Ramos <geaneramos1995@gmail.com>
2024-01-08 06:38:49 -0300

    feat: comments and pagination

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -6,12 +6,12 @@ import { useQuery } from "@tanstack/react-query";
 const maxPostPage = 10;
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
   const { data, isError, error, isLoading } = useQuery({
-    queryKey: ["posts"],
-    queryFn: fetchPosts,
+    queryKey: ["posts", currentPage],
+    queryFn: () => fetchPosts(currentPage),
     staleTime: 2000,
   });
   if (isLoading) {
@@ -41,11 +41,11 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button disabled={currentPage <= 1} onClick={() => {setCurrentPage((previousValue) => previousValue - 1)}}>
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button disabled={currentPage>=maxPostPage} onClick={() => {setCurrentPage((previousValue) => previousValue + 1)}}>
           Next page
         </button>
       </div>
